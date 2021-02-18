43b9a594ca3c362704c8f0ef62a81a1555f1a647
carol-kangara <carolkangara30@gmail.com>
2021-02-18 20:57:21 +0300

    add the method to run before each test

--- tests/credential_test.py
+++ tests/credential_test.py
@@ -5,8 +5,8 @@ class TestCredentials(unittest.TestCase):
     '''
     Test class that defines test cases for the contact class behaviour
     '''
-    # def setUp(self):
-    #     '''
-    #     the set up method to run before each code
-    #     '''
-    #     self.new_account=Credentials("twitter","karolk","uiaggscvbn")
+    def setUp(self):
+        '''
+        the set up method to run before each code
+        '''
+        self.new_account=Credentials("twitter","karolk","uiaggscvbn")

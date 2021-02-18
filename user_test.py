bb5ab5cea95a4b4cdccddc17d36417e40b1f949c
carol-kangara <carolkangara30@gmail.com>
2021-02-18 20:16:55 +0300

    add the __init__ method

--- user_test.py
+++ user_test.py
@@ -2,3 +2,10 @@ class User:
     '''
     This is a class that generates new instances
     '''
+    def __init__(self,user_name,password):
+        '''
+        we have created 3 arguments,the first argument is self.
+        Args:
+        self.user_name=user_name
+        self.password=password
+        '''

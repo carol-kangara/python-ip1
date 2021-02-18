2cd133d26b7f7178848d18a400598421342726ae
carol-kangara <carolkangara30@gmail.com>
2021-02-18 20:56:32 +0300

    add a subclass testcredentials

--- credentials.py
+++ credentials.py
@@ -2,10 +2,12 @@ class Credentials:
     '''
     This is a class that generates new instances of account credential storage
     '''
-    def __init__(self,account_name,account_password):
+    account_list=[] #empty account list
+    def __init__(self,account_name,account_user_name,account_password):
         '''
         we have created 3 arguments,the first argument is self.
         Args:
         self.account_name=account_name
+        self.account_user_name=account_user_name
         self.account_password=account_password
         '''
---
+++ tests/credential_test.py
@@ -0,0 +1,12 @@
+import unittest#importing the unit test module
+from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviour
+    '''
+    # def setUp(self):
+    #     '''
+    #     the set up method to run before each code
+    #     '''
+    #     self.new_account=Credentials("twitter","karolk","uiaggscvbn")

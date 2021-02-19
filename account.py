e9554536dab8a467b6a0d87575c35ec50c1dd88e
carol-kangara <carolkangara30@gmail.com>
2021-02-19 10:56:55 +0300

    add the main function

--- account.py
+++ account.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
-from credentials inport credentials
+from credentials import Credentials
+from user import Users
 def create_credentials(account_name,usern,password):
     '''
     function that creates credentials
@@ -32,4 +33,53 @@ def display_contacts():
     function that returns all the saved contacts
     '''
     return Credentials.display_credentials()
-def main():        
+def generate_password():
+	'''
+	Function to generate a password for a user
+	'''
+	gen_pass = Credential.generate_password()
+	return gen_pass    
+def main(): 
+    while True:
+        print("welcome to password locker")
+        print('.\n')
+        print("select a short code to navigate through:to create new user 'nu':to log in to your account 'l' or 'e' to exit")
+        short_code = input().lower() 
+        print('.\n')
+
+        if short_code =='nu':
+            print('create username')
+            created_user_name = input()
+
+            print('create password')
+            created_user_password = input()
+
+            print('confirm password')
+            confirm_password = input()
+
+            while confirm_password != created_user_password:
+                print("password did not match,")
+                print('enter your password')
+                created_user_password = input()
+                print('confirm your password')
+                confirm_password = input()
+            else:
+                print(f"Congratulations (created_user_name),account sucessfully created")
+                print('\n')
+                print("You can now log in")
+                print("username")
+                entered_username= input()
+                print("enter your password")
+                entered_password=input()
+            while entered_username != created_user_name or entered_password != created_user_password:
+                print("invalid username or password")
+                print("username")
+                entered_username=input()
+                print("your password")
+                entered_password=input()
+            else:
+                print(f"welcome:(entered-username) to you account")
+                print('\n') 
+        elif short_code =='l':          
+            print("welcome")
+            print("enter username")               
--- user.py
+++ user.py
@@ -1,7 +1,13 @@
 class Users:
     """
     class that generates new instances of users
-    """ 
+    """
+    def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+		'''
+		Function to generate an 8 character password for a credential
+		'''
+		gen_pass=''.join(random.choice(char) for _ in range(size))
+		return gen_pass 
     def __init__(self,user_name,password):
         self.user_name=user_name
         self.password=password

2273c3c851da121b1bdd2f08fa4c39a99c6d7f13
carol-kangara <carolkangara30@gmail.com>
2021-02-19 10:18:41 +0300

    add the user class instances

---
+++ user.py
@@ -0,0 +1,9 @@
+class Users:
+    """
+    class that generates new instances of users
+    """ 
+    def __init__(self,user_name,password):
+        self.user_name=user_name
+        self.password=password
+    def save-user(self): 
+        #used to add or to save instances of user to the user list

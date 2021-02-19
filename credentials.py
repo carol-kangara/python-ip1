f3ac265cbdcefa64afdf8c360b39331a7c54caef
carol-kangara <carolkangara30@gmail.com>
2021-02-19 06:38:15 +0300

    add the second text

--- credentials.py
+++ credentials.py
@@ -2,7 +2,12 @@ class Credentials:
     '''
     This is a class that generates new instances of account credential storage
     '''
-    account_list=[] #empty account list
+    credentials_list=[] #empty account list
+    def save_credentials():
+        '''
+        This i a function for saving credentials
+        '''
+    Credentials.credentials_list.append(self)
     def __init__(self,account_name,account_user_name,account_password):
         '''
         we have created 4 arguments,the first argument is self.
--- tests/credential_test.py
+++ tests/credential_test.py
@@ -9,14 +9,22 @@ class TestCredentials(unittest.TestCase):
         '''
         the set up method to run before each code
         '''
-        self.new_account=Credentials("twitter","karolk","uiaggscvbn")
+        self.new_credentials=Credentials("twitter","karolk","uiaggscvbn")
     def _init(self):
          '''
         Tests if the object is initialized properly
         '''
-        self.assertEqual(self.new_account.account_name,'twitter')
-        self.assertEqual(self.new_account.account_user_name,'karolk')
-        self.assertEqual(self.new_account.account_password,'uiaggscvbn')
+        self.assertEqual(self.new_credentials.account_name,'twitter')
+        self.assertEqual(self.new_credentials.account_user_name,'karolk')
+        self.assertEqual(self.new_credentials.account_password,'uiaggscvbn')
+    def test_save_credentials(self):
+        '''
+        Case test to checkif the account is saved into the credentials list
+        '''
+        self.new_credentials.save_credentials()
+        self.assertTrue(len(Credentials.credentials-list),1)
+
+
 if __name__ == '__main__':
     unittest.main()
           

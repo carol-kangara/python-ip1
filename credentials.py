f88a96e7fe9d922a3c3db9e352566123398a402f
carol-kangara <carolkangara30@gmail.com>
2021-02-19 07:09:22 +0300

    add the delet method in credentials.py

--- credentials.py
+++ credentials.py
@@ -3,11 +3,16 @@ class Credentials:
     This is a class that generates new instances of account credential storage
     '''
     credentials_list=[] #empty account list
-    def save_credentials():
+    def save_credentials(self):
         '''
-        This i a method for saving credentials
+        This is a method for saving added account
         '''
-    Credentials.credentials_list.append(self)
+        Credentials.credentials_list.append(self)
+    def delete_credentials(self):
+         '''
+        This is a method for deleting an aaccount that is no longer used
+        '''  
+
     def __init__(self,account_name,account_user_name,account_password):
         '''
         we have created 4 arguments,the first argument is self.
